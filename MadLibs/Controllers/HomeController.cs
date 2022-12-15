91547cb970a07703b403b7572818c402178d87e8
Alex Johnson <alex.johnson293@gmail.com>
2022-12-14 16:59:50 -0800

    add to badlib structure

--- MadLibs/Controllers/HomeController.cs
+++ MadLibs/Controllers/HomeController.cs
@@ -6,19 +6,55 @@ namespace MadLib.Controllers
   public class HomeController : Controller
   {
 
-    [Route("/result")]
-    public ActionResult Result(string noun, string adjective, string verb) 
+    [Route("/")]
+
+    public ActionResult Choose () { return View();}
+
+    [Route("/result-zoo")]
+    public ActionResult ResultZoo(string adj1, string adj2, string adj3, string adj4, string adj5, string verb1, string verb2, string verb3, string noun1, string noun2, string noun3, string adverb1) 
     {
       Story userStory = new Story();
-      userStory.Noun = noun;
-      userStory.Adjective = adjective;
-      userStory.Verb = verb;
+      userStory.Noun1 = noun1;
+      userStory.Noun2 = noun2;
+      userStory.Noun3 = noun3;
+      userStory.Adjective1 = adj1;
+      userStory.Adjective2 = adj2;
+      userStory.Adjective3 = adj3;
+      userStory.Adjective4 = adj4;
+      userStory.Adjective5 = adj5;
+      userStory.Verb1 = verb1;
+      userStory.Verb2 = verb2;
+      userStory.Verb3 = verb3;
+      userStory.Adverb1 = adverb1;
       return View(userStory);
     }
 
-    [Route("/")]
+    [Route("/result-aliens")]
+    public ActionResult ResultAliens(string noun, string adjective, string verb) 
+    {
+      Story userStory = new Story();
+      userStory.Noun1 = noun1;
+      userStory.Noun2 = noun2;
+      userStory.Noun3 = noun3;
+      userStory.Adjective1 = adj1;
+      userStory.Adjective2 = adj2;
+      userStory.Adjective3 = adj3;
+      userStory.Adjective4 = adj4;
+      userStory.Adjective5 = adj5;
+      userStory.Verb1 = verb1;
+      userStory.Verb2 = verb2;
+      userStory.Verb3 = verb3;
+      userStory.Adverb1 = adverb1;
+      return View(userStory);
+    }
 
-    public ActionResult Form () { return View();}
+    [Route("/form-zoo")]
+
+    public ActionResult FormZoo () { return View();}
+
+    [Route("/form-aliens")]
+
+    public ActionResult FormAliens () { return View();}
 
   }
 }
--- MadLibs/Models/Story.cs
+++ MadLibs/Models/Story.cs
@@ -2,8 +2,25 @@ namespace MadLib.Models
 {
   public class Story
   {
-    public string Noun { get; set; }
-    public string Adjective { get; set; }
-    public string Verb { get; set; }
+    public string Noun1 { get; set; }
+    public string Adjective1 { get; set; }
+    public string Verb1 { get; set; }
+    public string Adverb1 { get; set; }
+    public string Noun2 { get; set; }
+    public string Adjective2 { get; set; }
+    public string Verb2 { get; set; }
+    public string Adverb2 { get; set; }
+    public string Noun3 { get; set; }
+    public string Adjective3 { get; set; }
+    public string Verb3 { get; set; }
+    public string Adverb3 { get; set; }
+    public string Noun4 { get; set; }
+    public string Adjective4 { get; set; }
+    public string Verb4 { get; set; }
+    public string Adverb4 { get; set; }
+    public string Noun5 { get; set; }
+    public string Adjective5 { get; set; }
+    public string Verb5 { get; set; }
+    public string Adverb5 { get; set; }
   }
 }
